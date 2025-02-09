ef1b5cdc99128d2b3eec34cdcef764cbae41d9f1
Linar <linarus08@mail.ru>
2025-02-09 13:02:24 +0500

    add editText - refactoring

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import { getPackages } from "./utils/packages.js";
 import { getAllFiles } from "./utils/files.js";
 import { INPUT_FOLDER, OUTPUT_FOLDER, EXTENSION } from "./config.js";
-import { text_processing } from "./utils/edit_text.js";
+import { textProcessing } from "./utils/edit_text.js";
 
 async function main() {
   let packages = getPackages(INPUT_FOLDER); // список пакетов из папки input
-  text_processing(packages);
+  textProcessing(packages);
 }
 
 main();
--- utils/edit_text.js
+++ utils/edit_text.js
@@ -4,65 +4,39 @@ import fs from "fs";
 import { INPUT_FOLDER, OUTPUT_FOLDER } from "../config.js";
 import path from "path";
 
-function edit_text(pack) {
-  let full_name_pack = path.join(INPUT_FOLDER, pack);
-  fs.readFile(full_name_pack, (err, data) => {
-    if (err) throw err;
-    const zip = new JSZip();
-    // Загружаем ZIP-архив
-    zip
-      .loadAsync(data)
-      .then((zip) => {
-        // Получаем список файлов в архиве
-        const files = Object.keys(zip.files);
-        // Проходим по каждому файду
-        const promises = files.map((fileName) => {
-          // Проверяем, является ли файл html
-          if (fileName.endsWith(".html")) {
-            return zip
-              .file(fileName)
-              .async("string")
-              .then((content) => {
-                // Изменям содержимое файла в архиве
-                const newContent = content.replace(
-                  "&#x420;&#x410;&#x421;&#x41f;&#x41e;&#x420;&#x42f;&#x416;&#x415;&#x41d;&#x418;&#x415;",
-                  "qwer"
-                );
-                // Обновляем файл в архиве
-                zip.file(fileName, newContent);
-              });
-          }
-          return Promise.resolve(); // Если файл не текстовый, просто возвращаем resolved promise
-        });
+async function editText(pack) {
+  const fullNamePack = path.join(INPUT_FOLDER, pack);
 
-        return Promise.all(promises);
-      })
-      .then(() => {
-        // Сохраняем измененный ZIP-архив
-        return zip.generateAsync({ type: "nodebuffer" });
-      })
-      .then((content) => {
-        return new Promise((resolve, rejects) => {
-          let new_pack = path.join(OUTPUT_FOLDER, pack)
-          fs.writeFile(new_pack, content, (err) => {
-            if (err) reject(err);
-            else {
-              console.log("ZIP-архив успешно обновлен!");
-              resolve();
-            }
-          });
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  });
-}
+  try {
+    const data = await fs.promises.readFile(fullNamePack);
+    const zip = await JSZip.loadAsync(data);
 
-function text_processing(packages) {
-  for (const pack of packages) {
-    edit_text(pack);
+    const files = Object.keys(zip.files);
+    const promises = files.map(async (fileName) => {
+      if (fileName.endsWith(".html")) {
+        const content = await zip.file(fileName).async("string");
+        const newContent = content.replace(
+          "&#x420;&#x410;&#x421;&#x41f;&#x41e;&#x420;&#x42f;&#x416;&#x415;&#x41d;&#x418;&#x415;",
+          "qwer"
+        );
+        zip.file(fileName, newContent);
+      }
+    });
+    await Promise.all(promises);
+
+    const content = await zip.generateAsync({ type: "nodebuffer" });
+    const newPack = path.join(OUTPUT_FOLDER, pack);
+    await fs.promises.writeFile(newPack, content);
+    console.log("ZIP-архив успешно обновлен!");
+  } catch (err) {
+    console.error(err);
   }
 }
 
-export { text_processing };
+async function textProcessing(packages) {
+  for (const pack of packages) {
+    await editText(pack);
+  }
+}
+
+export { textProcessing };
